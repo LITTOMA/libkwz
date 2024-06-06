28b88ce5c75d6e59275da056490d28133031563a
LIT <littoma@hotmail.com>
2024-06-06 20:23:29 +0800

    feat: Add KwzThumbnail class for reading and writing thumbnail data

---
+++ src/KwzThumbnail.cs
@@ -0,0 +1,40 @@
+using System.IO.Hashing;
+
+namespace LibKwz;
+
+public class KwzThumbnail
+{
+    private char[] _magic = new char[4];
+    private uint _size;
+    private uint _crc32;
+    private byte[] _data;
+
+    public char[] Magic => _magic;
+    public uint Size => _size;
+    public uint Crc32 { get => _crc32; set => _crc32 = value; }
+    public byte[] Data { get => _data; set => _data = value; }
+
+    public static KwzThumbnail ReadFrom(BinaryReader reader)
+    {
+        var thumbnail = new KwzThumbnail();
+        reader
+            .ReadChars(4, ref thumbnail._magic, Encoding.ASCII)
+            .ReadUInt32(ref thumbnail._size)
+            .ReadUInt32(ref thumbnail._crc32);
+
+        thumbnail._data = reader.ReadBytes((int)thumbnail._size);
+        return thumbnail;
+    }
+
+    public void WriteTo(BinaryWriter writer)
+    {
+        _size = (uint)_data.Length;
+        _crc32 = System.IO.Hashing.Crc32.HashToUInt32(_data);
+
+        writer
+            .WriteValue([.."KTN\x02"], Encoding.ASCII)
+            .WriteValue(_size)
+            .WriteValue(_crc32)
+            .WriteValue(_data);
+    }
+}
